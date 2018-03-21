f6ba695b75864d5eaaad2a960c1c185d6f3b724b
Shawn Toffel <shawn.toffel@gmail.com>
2018-03-20 21:13:17 -0600

    Import birthday job

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jukeizu/birthday/job"
+	"github.com/shawntoffel/services-core/command"
+	configreader "github.com/shawntoffel/services-core/config"
+	"github.com/shawntoffel/services-core/logging"
+)
+
+var commandArgs command.CommandArgs
+
+type Config struct {
+	JobConfig job.Config
+}
+
+func init() {
+	commandArgs = command.ParseArgs()
+}
+
+func main() {
+	logger := logging.GetLogger("birthday", os.Stdout)
+
+	config := Config{}
+
+	err := configreader.ReadConfig(commandArgs.ConfigFile, &config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = job.Start(logger, config.JobConfig)
+
+	if err != nil {
+		panic(err)
+	}
+}
